43fcb9027b499f64a58df4dee8e9cc80931f141a
Anjith Padmanabhan <46625878+anjith101@users.noreply.github.com>
2024-05-07 19:21:19 +0530

    Add files via upload

---
+++ login-flow-playwright.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  console.log('this is env: ', process.env)
+  await page.goto('https://practicetestautomation.com/');
+  await page.screenshot({ path: "/home/uncloud/monitorings/screenshots/screenshot1.png"});
+  await page.getByRole('link', { name: 'Practice', exact: true }).click();
+  await page.getByRole('link', { name: 'Test Login Page' }).click();
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill('student');
+  await page.screenshot({ path: "/home/uncloud/monitorings/screenshots/screenshot2.png"});
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('Password123');
+  await page.screenshot({ path: "/home/uncloud/monitorings/screenshots/screenshot3.png"});
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await page.screenshot({ path: "/home/uncloud/monitorings/screenshots/screenshot4.png"});
+  await page.getByRole('link', { name: 'Log out' }).click();
+  await page.screenshot({ path: "/home/uncloud/monitorings/screenshots/screensho41.png"});
+  await page.getByRole('link', { name: 'Courses' }).click();
+  await page.screenshot({ path: "/home/uncloud/monitorings/screenshots/screenshot5.png"});
+  await page.getByRole('link', { name: 'Blog' }).click();
+  await page.screenshot({ path: "/home/uncloud/monitorings/screenshots/screenshot6.png"});
+  await page.getByRole('link', { name: 'Contact' }).click();
+  await page.screenshot({ path: "/home/uncloud/monitorings/screenshots/screenshot7.png"});
+  await page.getByRole('link', { name: 'Home' }).click();
+  await page.screenshot({ path: "/home/uncloud/monitorings/screenshots/screenshot8.png"});
+});
