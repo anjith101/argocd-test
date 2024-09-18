7d886fa55882dc4a6f7d1481e61c09dcb6bd45df
Anjith Padmanabhan <46625878+anjith101@users.noreply.github.com>
2024-09-18 23:33:40 +0530

    Update test.spec.ts

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  console.log('this is the updated script');
+  console.log('this is the updated script v1');
   await page.goto('https://practicetestautomation.com/');
   await page.screenshot({ path: "/home/uncloud/monitorings/screenshots/screenshot1.png"});
   await page.getByRole('link', { name: 'Practice', exact: true }).click();
