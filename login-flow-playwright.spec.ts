a9466340659674791bfadeded67aafbdfc982ad9
Anjith Padmanabhan <46625878+anjith101@users.noreply.github.com>
2024-09-23 18:07:40 +0530

    Update login-flow-playwright.spec.ts

--- login-flow-playwright.spec.ts
+++ login-flow-playwright.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  console.log('this is the updated script');
+  console.log('this file has been edited for testing v1');
   await page.goto('https://practicetestautomation.com/');
   await page.screenshot({ path: "/home/uncloud/monitorings/screenshots/screenshot1.png"});
   await page.getByRole('link', { name: 'Practice', exact: true }).click();
